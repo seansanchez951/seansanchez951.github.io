6eadeade8e6aced91460b1967f85148440420fbb
seansanchez951 <69922895+seansanchez951@users.noreply.github.com>
2022-04-19 12:36:57 -0700

    Update bubble-chart.js

--- reviewingpitchforkreviews-2022-datavis/js/bubble-chart.js
+++ reviewingpitchforkreviews-2022-datavis/js/bubble-chart.js
@@ -7,7 +7,7 @@ function main() {
         height = 480 - margin.top - margin.bottom;
 
     // append the svg object to the body of the page
-    var svg = d3.select("#my_dataviz")
+    var svg = d3.select("#bubble-chart")
         .append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)

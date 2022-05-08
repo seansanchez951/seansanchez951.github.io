3c728155e407e68bd9cf12fe4bbfbc0bc7ec61ca
seansanchez951 <69922895+seansanchez951@users.noreply.github.com>
2022-05-08 08:58:33 -0700

    Update treemap2.js

--- reviewingpitchforkreviews/assets/js/treemap2.js
+++ reviewingpitchforkreviews/assets/js/treemap2.js
@@ -18,8 +18,8 @@ function main() {
 
 
     Promise.all([
-        d3.csv("data/genre-average-score.csv"),
-        d3.csv("data/top-five-albums.csv")
+        d3.csv("./data_files/genre-average-score.csv"),
+        d3.csv("./data_files/top-five-albums.csv")
     ]).then(([treeData,toolData]) => {
 
         // console.log(treeData);

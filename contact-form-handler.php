df19cbbd711ea341cbe889b833c5c75cb41237d1
seansanchez951 <69922895+seansanchez951@users.noreply.github.com>
2022-06-09 15:22:30 -0700

    Update contact-form-handler.php

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -3,7 +3,7 @@
 	$visitor_email = $_POST['email'];
 	$message = $_POST['message'];
 
-	$email_from = $visitor_email;
+	$email_from = 'seansanchez951@gmail.com';
 
 	$email_subject = 'New Form Submission';
 
